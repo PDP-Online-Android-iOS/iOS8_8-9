7a81cfbaa4510083c5e367dd7512bd334ef5e21b
OgabekDev <ogabekdev@gmail.com>
2023-01-14 09:37:32 +0500

    Initial Commit

---
+++ UnitUITesting/UnitUITestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitUITestingApp.swift
+//  UnitUITesting
+//
+//  Created by Ogabek Matyakubov on 14/01/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitUITestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
